7c241208dd75b82a4f809fc43bade356fd766dea
markuszilch <zilchmarkus@web.de>
2024-02-13 13:43:40 +0100

    install epel-release for all RedHat distro tests

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -3,5 +3,5 @@ require 'voxpupuli/acceptance/spec_helper_acceptance'
 configure_beaker do |host|
   install_local_module_on(host)
   install_puppet_module_via_pmt_on(host, 'puppetlabs-stdlib')
-  install_package(host, 'epel-release') if fact_on(host, 'os.name') == 'CentOS'
+  install_package(host, 'epel-release') if fact_on(host, 'os.family') == 'RedHat'
 end
