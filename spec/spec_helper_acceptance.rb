f3d0ce7a560995fe1dbcab29c432cc4eaa790586
markuszilch <zilchmarkus@web.de>
2024-02-13 01:25:28 +0100

    fix stdlib install

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -2,5 +2,5 @@ require 'voxpupuli/acceptance/spec_helper_acceptance'
 
 configure_beaker do |host|
   install_local_module_on(host)
-  install_puppet_module_via_pmt_on(host, stdlib)
+  install_puppet_module_via_pmt_on(host, 'puppetlabs-stdlib')
 end
