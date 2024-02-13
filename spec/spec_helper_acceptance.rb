ae5d6e870a927f4bb809be5d286dd5c06c11d0f4
markuszilch <zilchmarkus@web.de>
2024-02-13 01:13:21 +0100

    dont explicitly install puppet agent on host

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -1,8 +1,6 @@
 require 'voxpupuli/acceptance/spec_helper_acceptance'
 
 configure_beaker do |host|
-  # Install Puppet
-  install_puppet_release_repo_on(host)
-  install_local_module on(host)
+  install_local_module_on(host)
   install_puppet_module_via_pmt_on(host, stdlib)
 end
