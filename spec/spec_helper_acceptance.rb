1e9c1931f93f7c1e83f91ab2e468eba570bbe4d8
markuszilch <zilchmarkus@web.de>
2024-02-13 01:08:49 +0100

    replace spec_helper_acceptance with something more reasonable

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -1,19 +1,8 @@
-require 'beaker_puppet_helpers'
+require 'voxpupuli/acceptance/spec_helper_acceptance'
 
-hosts.each do |host|
+configure_beaker do |host|
   # Install Puppet
   install_puppet_release_repo_on(host)
-end
-
-RSpec.configure do |c|
-  c.formatter = :documentation
-
-  # Configure all nodes in nodeset
-  c.before :suite do
-    # Install module
-    install_local_module on(host)
-    hosts.each do |host|
-      install_puppet_module_via_pmt_on(host, stdlib)
-    end
-  end
+  install_local_module on(host)
+  install_puppet_module_via_pmt_on(host, stdlib)
 end
