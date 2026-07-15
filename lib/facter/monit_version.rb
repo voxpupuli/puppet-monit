92869ac0c503cedff08de62e44bfde322c632bef
Erik Geiger <erik@frontify.com>
2026-07-15 09:45:15 +0200

    fix: Adjust monit version detect fact
    The facter code did not detect versions with 2 dots, fixed the
    detection so it works with 2 and 3 digits.

--- lib/facter/monit_version.rb
+++ lib/facter/monit_version.rb
@@ -3,6 +3,6 @@
 Facter.add(:monit_version) do
   setcode do
     monit_version = Facter::Util::Resolution.exec('monit -V 2>&1')
-    monit_version && monit_version.match(%r{\d+\.\d+$}).to_s
+    monit_version && monit_version.match(%r{\d+\.\d+(?:\.\d+)?$}).to_s
   end
 end
